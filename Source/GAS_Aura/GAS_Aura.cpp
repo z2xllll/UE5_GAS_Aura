da4bc7b82473877a9e869cd291ee84450d474f8d
Xiulong Zhang <414869369@qq.com>
2025-07-15 00:33:04 +0800

    first commit

---
+++ Source/GAS_Aura/GAS_Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAS_Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAS_Aura, "GAS_Aura" );
